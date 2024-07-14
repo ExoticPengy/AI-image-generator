4e435f89adcf3b290b19c7c5c47ff3a7c24d1d57
ExoticPengy <157903768+ExoticPengy@users.noreply.github.com>
2024-07-14 21:26:26 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const modelslabApiKey = process.env.MODELSLAB
+
+import express, { response } from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+app.post('/dream', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        let raw = JSON.stringify({
+            "key" : modelslabApiKey,
+            "prompt": prompt,
+            "negative_prompt": "bad quality",
+            "width": "512",
+            "height": "512",
+            "safety_checker": false,
+            "seed": null,
+            "samples":1,
+            "base64":false,
+            "webhook": null,
+            "track_id": null
+        });
+        
+        let requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+    
+        const aiResponse = await fetch("https://modelslab.com/api/v6/realtime/text2img", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    
+        console.log(aiResponse)
+    
+        const image = data.output[0];
+        res.send({ image });
+    } catch (error) {
+        console.error(error)
+        res.status(500).send("Something went wrong.")
+    }
+});
+
+app.listen(8080, () => console.log('make art on https://localhost:8080/dream'))
